6022bf96d80ccf93d1e734cd27b5d7f76e2c0356
DeadrixMDS <alexandre.bouriche@my-digital_school.org>
2023-10-01 18:46:55 +0200

    Création des routes pour utiliser twig

---
+++ src/routes/Portfolio.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader('../templates');
+$twig = new Environment($loader);
+
+echo $twig->render('Portfolio.twig');
